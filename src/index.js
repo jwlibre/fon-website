5a12c745788ae7f04b4f79f3dadb3705b1a3acab
SeanPayneArchDesign <74615707+SeanPayneArchDesign@users.noreply.github.com>
2021-12-31 16:49:05 +0000

    Refactor code. Add raycaster. visibility groups

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import * as THREE from 'three';
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {FlyControls} from "three/examples/jsm/controls/FlyControls";
 
 let mesh, groundMesh;
-let renderer;
-let scene;
-let camera, controls;
+let scene, camera, controls, renderer, raycaster, INTERSECTED;
+let fieldGroup, room1Group, room2Group;
+let pointer = new THREE.Vector2();
 
 let targetX = 0;
 let targetY = 0;
@@ -22,71 +21,105 @@ window.addEventListener( 'resize', onWindowResize, false );
 init();
 animate();
 
+function createFieldScene(){
+
+        /**
+         * I've created this function to isolate the scene creation from init(), this will make more sense and help workflow once we're using
+         * the GLTFLoader to insert custom 3D models and give us some flexibility regarding when models are loaded 
+         * (e.g. all at the beginning or when we enter a new room). We can use eventlistener tirggered arguments in this function to trigger 
+         * the correct filename load and call the audio load function as well, which should greatly reduce our code
+         * 
+         * for now it just makes the same shit we already had :)
+         */
+
+        // create field scene group (represents the GLTF scene import for now, I'll explain if you have questions)
+        fieldGroup = new THREE.Group();
+
+        // Adding lighting
+        const fieldAmbientLight = new THREE.AmbientLight( 'white', 0.4 );
+        const fieldDirectionalLight = new THREE.DirectionalLight( 'yellow', 0.8);
+        fieldDirectionalLight.position.set(3, 4, 3);
+    
+        // build doors with a lazy layout - doors will need specific names in your 3D model scene
+        const doorGeometry = new THREE.BoxGeometry( 0.8, 1.9, 0.08 );
+        const doorMaterial = new THREE.MeshStandardMaterial();
+    
+        for (let i = 0; i < 10; i++) {
+            const door = new THREE.Mesh( doorGeometry, doorMaterial );
+            door.position.x = i - 5;
+            door.position.z = - 3;
+            fieldGroup.add(door);
+          }
+        
+        // build floor
+        const groundGeometry = new THREE.CylinderBufferGeometry(30, 30, 0.5, 32, 1); 
+        groundMesh = new THREE.Mesh( groundGeometry, doorMaterial );
+        groundMesh.position.y = -0.7
+
+        fieldGroup.add( fieldAmbientLight, fieldDirectionalLight, groundMesh );
+
+        scene.add( fieldGroup );
+        console.log( fieldGroup );
+}
+
+
+function fieldSceneControls(){
+    camera.rotation.y += 0.03 * ( - targetX - camera.rotation.y );
+    camera.rotation.x += 0.03 * ( - targetY - camera.rotation.x );
+    camera.rotation.z = 0;
+}
+
+
+function castingRay() {
+
+    // find intersections
+
+    raycaster.setFromCamera( pointer, camera );
+
+    const intersects = raycaster.intersectObjects( scene.children );
+
+    if (intersects.length > 0) {
+
+        console.log("intersection");
+        // target the correct item in a mesh and add desired material change etc.
+
+	}
+
+}
+
+
 function init() {
 
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 50 );
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 50 );
     camera.position.z = 1;
 
     scene = new THREE.Scene();
+    createFieldScene();
 
-    // Adding ambient light
-    const ambientLight = new THREE.AmbientLight( 'white', 0.4 );
-    scene.add(ambientLight);
-
-    // add direction light
-    const directionalLight = new THREE.DirectionalLight( 'yellow', 0.8);
-    directionalLight.position.set(3, 4, 3);
-    scene.add(directionalLight);
-
-    // build basic door
-    const geometry = new THREE.BoxGeometry( 0.8, 1.9, 0.08 );
-    const material = new THREE.MeshStandardMaterial();
-
-    const doorMesh1 = new THREE.Mesh( geometry, material );
-    const doorMesh2 = new THREE.Mesh( geometry, material );
-    const doorMesh3 = new THREE.Mesh( geometry, material );
-
-    doorMesh1.position.set( 1, 0, 0 );
-    doorMesh2.position.set( 0, 0, 0 );
-    doorMesh3.position.set( -1, 0, 0 );
-
-    scene.add( doorMesh1, doorMesh2, doorMesh3 );
-
-    // build floor
-    const groundGeometry = new THREE.CylinderBufferGeometry(30, 30, 0.5, 32, 1);
-
-    groundMesh = new THREE.Mesh( groundGeometry, material );
-    groundMesh.position.y = -0.7
-    scene.add( groundMesh );
-
+    //raycaster
+    raycaster = new THREE.Raycaster();
+    
+    //renderer
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-
+    //camera controls
     controls = new FlyControls( camera, renderer.domElement );
     controls.movementSpeed = 0;
-    controls.domElement = renderer.domElement;
     controls.rollSpeed = Math.PI / 10;
-    controls.autoForward = false;
-    controls.dragToLook = false;
-
-    controls.minAzimuthAngle = -1; // default
-    controls.maxAzimuthAngle = 1; // default
-    
-    controls.minPolarAngle = 0; // default
-    controls.maxPolarAngle = 0; // default
-
 
     document.addEventListener( 'mousemove', onDocumentMouseMove );
 }
 
+
 function onDocumentMouseMove( event ) {
 
-    mouseX = ( event.clientX - windowHalfX );
-    mouseY = ( event.clientY - windowHalfY );
+    pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
 }
+
 
 function animate() {
 
@@ -96,24 +129,28 @@ function animate() {
     //mesh.rotation.y += 0;
 
     render();
-
 }
+
 
 function render() {
 
     const delta = clock.getDelta();
 
-    targetX = mouseX * .001;
-    targetY = mouseY * .001;
+    targetX = pointer.x * .1;
+    targetY = pointer.y * .1;
 
-    camera.rotation.y += 0.08 * ( - targetX - camera.rotation.y );
-    camera.rotation.x += 0.08 * ( - targetY - camera.rotation.x );
-    camera.rotation.z = 0;
+    if(fieldGroup.visible == true) {
+        fieldSceneControls();
+        //fieldSceneAudio();
+        // other stuff in field scene, particles etc
+    }
+
+    castingRay();
     
     renderer.render( scene, camera );
     controls.update( delta );
-
 }
+
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
